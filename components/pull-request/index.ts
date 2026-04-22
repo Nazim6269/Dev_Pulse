44232a6c8f34bc7dac8695639e8df13ee9109522
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-22 08:32:05 +0600

    feat(pull-request): add barrel export in pull request

---
+++ components/pull-request/index.ts
@@ -0,0 +1,7 @@
+export * from "@/components/pull-request/ActionDropdown";
+export * from "@/components/pull-request/PullRequestHeader";
+export * from "@/components/pull-request/PullRequestItem";
+export * from "@/components/pull-request/PullRequestList";
+export * from "@/components/pull-request/PullRequestsDashboard";
+export * from "@/components/pull-request/ReviewerAvatars";
+export * from "@/components/pull-request/StatusBadge";
