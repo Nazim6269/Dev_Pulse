6e732e6a3d0c443a8662c65d452c5b9ba39b96d3
Nazim6269 <nazimdev10022001@gmail.com>
2026-05-15 10:34:18 +0600

    feat: add goal tracker data

--- widgets/dashboard/ui/home/skeleton/index.ts
+++ widgets/dashboard/ui/home/skeleton/index.ts
@@ -3,4 +3,5 @@ export { default as TopReposSkeleton } from "./TopReposSkeleton";
 export { default as RecentPRsSkeleton } from "./RecentPRsSkeleton";
 export { default as StatsRowSkeleton } from "./StatsRowSkeleton";
 export { default as PRCycleCardSkeleton } from "./PRCycleCardSkeleton";
+export { default as GoalTrackerSkeleton } from "./GoalTrackerSkeleton";
 
